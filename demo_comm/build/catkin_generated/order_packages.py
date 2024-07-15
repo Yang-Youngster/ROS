cc25f2c4ff5a0175e6142b7193fde378ed7ef0e1
Yang-Youngster <yangyoungster@163.com>
2024-07-15 15:18:21 +0800

    通信机制demo

---
+++ demo_comm/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/luoyang/桌面/ROS/demo_comm/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/luoyang/桌面/ROS/demo_comm/devel;/opt/ros/noetic'.split(';') if '/home/luoyang/桌面/ROS/demo_comm/devel;/opt/ros/noetic' != '' else []
